694a60f54907ad2f4f3fa6a961bed39ccd7c19a6
jlaportetn <jlaporte81@gmail.com>
2016-01-06 14:21:16 -0600

    Added Ex2

---
+++ Lect55Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect55Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+
+            var name = Console.ReadLine();
+            char[] nameAsArray = name.ToCharArray();
+            Array.Reverse(nameAsArray);
+            {
+                Console.Write(nameAsArray);
+                Console.ReadKey();
+            }
+        }
+        }
+    }
+
