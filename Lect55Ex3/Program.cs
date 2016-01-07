317f7fff7b18a85666a6813f3b7980a1aad55d86
jlaportetn <jlaporte81@gmail.com>
2016-01-07 17:37:24 -0600

    Added Ex3

---
+++ Lect55Ex3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect55Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 5 numbers with no duplicates.");
+            var numbers =(Console.ReadLine());
+            var numbersList = new List<int>();
+            numbersList = numbers.Split(' ').Select(Int32.Parse).ToList();
+            List<int> Distinct = numbersList.Distinct().ToList();
+            Distinct.Sort();
+
+            if (Distinct.Count < 5) 
+            {
+                Console.WriteLine("Duplicate entered, or not enough numbers entered.  Please try again.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Sorted numbers");
+                {
+                    foreach (int i in Distinct)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
