da673395ba46123c911b71c4b3a5cd698d918900
jlaportetn <jlaporte81@gmail.com>
2016-01-09 20:22:49 -0600

    Added Ex4

---
+++ Lect55Ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect55Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sequence of numbers, or type quit to exit.");
+            var numbers = (Console.ReadLine());
+
+            if (numbers == "quit")
+            {
+                Console.ReadKey();
+            }
+            else
+            {
+                var numbersList = new List<int>();
+                numbersList = numbers.Split(' ').Select(Int32.Parse).ToList();
+                List<int> Distinct = numbersList.Distinct().ToList();
+
+                foreach (int i in Distinct)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
