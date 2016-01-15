a6e6d6b11e87a9c953815fbfac7afb833e4e27a3
jlaportetn <jlaporte81@gmail.com>
2016-01-15 17:18:12 -0600

    Added Ex5

---
+++ Lect55Ex5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect55Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please entera series of 5 or more numbers, sseparated by a comma." );
+            var input = Console.ReadLine();
+            var numbers = new List<int>();
+            numbers = input.Split(',').Select(Int32.Parse).ToList();
+
+            if (numbers.Count < 5)
+            {
+                Console.WriteLine("Not enough numbers, try again");
+            }
+            else
+            {
+                numbers.Sort(); 
+                Console.WriteLine("The three smallest numbers are " + numbers[0] + numbers[1] + numbers[2]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lect55Ex_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect55Ex_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please entera series of 5 or more numbers, separated by a comma.");
+            var input = Console.ReadLine();
+            var numbers = new List<int>();
+            numbers = input.Split(',').Select(Int32.Parse).ToList();
+
+            if (numbers.Count < 5)
+            {
+                Console.WriteLine("Not enough numbers, try again");
+            }
+            else
+            {
+                numbers.Sort();
+                Console.WriteLine("The three smallest numbers are " + numbers[0] + ", " + numbers[1] + ", " + "and " + numbers[2]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
