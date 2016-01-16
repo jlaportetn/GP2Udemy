4efca8b38b83ec3d91c257870e5c36f0323a0895
jlaportetn <jlaporte81@gmail.com>
2016-01-16 16:05:10 -0600

    Added Lect67Ex3

---
+++ Lect67Ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect67Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a time using the 24 hour format.");
+            var userTime = Console.ReadLine();
+            TimeSpan time;
+
+            if (TimeSpan.TryParse(userTime, out time))
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.ReadKey();
+        }
+    }
+}
