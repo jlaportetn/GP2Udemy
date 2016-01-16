999a5fd2ba43c74ddd6558386865732a9b4c71fa
jlaportetn <jlaporte81@gmail.com>
2016-01-15 20:29:16 -0600

    Added Lect67Ex1

---
+++ Lect67Ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect67Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a series of numbers separated by a hyphen");
+            var numbers = Console.ReadLine();
+            var numbersList = new List<int>();
+            numbersList = numbers.Split('-').Select(Int32.Parse).ToList();
+            var sortedNumbers = new List<int>(numbersList);
+            sortedNumbers.Sort();
+        
+            if (numbersList.SequenceEqual(sortedNumbers))
+            {
+                Console.WriteLine("Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Not consecutive");
+            }
+            Console.ReadKey();
+        }
+    }
+}
