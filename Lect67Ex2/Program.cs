e2e946e072bc139f15df1ba2977b17248a306368
jlaportetn <jlaporte81@gmail.com>
2016-01-16 14:59:55 -0600

    Added Lect67Ex2

---
+++ Lect67Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect67Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a series of numbers separated by a hyphen");
+            var numbers = Console.ReadLine();
+            var numbers2 = string.Copy(numbers);
+
+            if (string.IsNullOrEmpty(numbers))
+            {
+                Console.WriteLine("Nothing entered.");
+            }
+            var numbersList = new List<int>();
+            numbersList = numbers2.Split('-').Select(Int32.Parse).ToList();
+            List<int> checkForDup = numbersList.Distinct().ToList();
+
+            if (numbersList.Count == checkForDup.Count)
+            {
+                Console.WriteLine("No duplicates entered.");
+            }
+            else
+            {
+                Console.WriteLine("Duplicate entered.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
