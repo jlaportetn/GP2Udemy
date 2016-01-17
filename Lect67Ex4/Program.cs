38cd9d5d72b9fb2907c0aaaaa3b55ecbefc30e5a
jlaportetn <jlaporte81@gmail.com>
2016-01-16 18:38:19 -0600

    Added Lect67Ex4

---
+++ Lect67Ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect67Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a series of words.");
+            var userEntry = Console.ReadLine();
+            var entryAsList = new List<string>();
+            entryAsList = userEntry.Split(' ').ToList();
+
+            foreach (var word in entryAsList)
+            {
+               var pascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1); 
+               Console.Write(pascalCase);
+            }
+            Console.ReadKey();
+        }
+    }
+}
