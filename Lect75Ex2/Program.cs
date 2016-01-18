f17f17dda55dcb2ce591dc0ab470e14866687368
jlaportetn <jlaporte81@gmail.com>
2016-01-17 18:34:47 -0600

    Added Lect75Ex2

---
+++ Lect75Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lect75Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintLongestWord();
+        }
+        static void PrintLongestWord()
+        {
+            var path = @"C:\Udemytext\sometext.txt";
+            var text = File.ReadAllText(path);
+            var words = text.Split(' ');
+            var longestWord = "";
+
+            foreach (var word in words)
+            {
+                if (word.Length > longestWord.Length)
+                    longestWord = word;
+            }
+            Console.WriteLine("The longest word is " + longestWord + ".");
+            Console.ReadKey();
+
+        }
+    }
+}
