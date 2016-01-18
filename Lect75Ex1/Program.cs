616ec8b4d905426895f26281cf80e6c3c27b4a8b
jlaportetn <jlaporte81@gmail.com>
2016-01-17 18:18:43 -0600

    Added Lect75Ex1

---
+++ Lect75Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lect75Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberOfWords();
+        }
+        static void NumberOfWords()
+        {
+            var path = @"C:\Udemytext\sometext.txt";
+            var readText = File.ReadAllText(path);
+            var words = readText.Split(' ');
+
+            if (!File.Exists(path))
+                return;
+            else
+            {
+                Console.WriteLine("The number of words is " +words.Length);
+            }
+        }
+    }
+}
